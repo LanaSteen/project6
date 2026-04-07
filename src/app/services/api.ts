85912840633716e39cbd6dad698947f9bfb9be1e
LanaNew <l4n.steen@gmail.com>
2026-04-07 20:57:43 +0400

    http + service

---
+++ src/app/services/api.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api {
+  constructor(private http: HttpClient) {}
+  // https://restaurant.stepprojects.ge/api/Products/GetAll
+  // https://restaurant.stepprojects.ge/api/Categories/GetAll
+
+  baseUrl = 'https://restaurant.stepprojects.ge/api/';
+  baseUrl2 = 'https://restaurantapi.stepacademy.ge/api/';
+  apiKey = '14c34c99-91b6-41a8-ad96-f4d3dc43e35b';
+
+  getAll(url: string) {
+    return this.http.get(this.baseUrl + url);
+  }
+
+  getAll2(url: string) {
+    return this.http.get(this.baseUrl2 + url, {
+      headers: {
+        'X-API-KEY': this.apiKey,
+      },
+    });
+  }
+}
+
+// http client     --  fetch
